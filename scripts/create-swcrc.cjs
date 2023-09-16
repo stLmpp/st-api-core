60d1d37022d757fc2281962359cc8874a0cf3176
stLmpp <gui.stlmpp@gmail.com>
2023-09-16 12:52:21 -0300

    clean up

--- scripts/create-swcrc.cjs
+++
@@ -1,5 +0,0 @@
-const { writeFile } = require('node:fs/promises');
-const { SWC_DEFAULT } = require('./constants.cjs');
-(async () => {
-  await writeFile('.swcrc', JSON.stringify(SWC_DEFAULT()));
-})();
