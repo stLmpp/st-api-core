f3cb2d00b4e157efa9b154b467b5540e05119550
stLmpp <gui.stlmpp@gmail.com>
2023-09-24 17:23:54 -0300

    feat: export vitest.setup.js to be used in all functions chore: bump minor version

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,4 +3,14 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   ...tsupConfig,
+  entry: {
+    index: './src/index.ts',
+    'vitest.setup': './vitest.setup.ts',
+  },
+  dts: {
+    entry: {
+      index: './src/index.ts',
+    },
+  },
+  external: ['vitest'],
 });
