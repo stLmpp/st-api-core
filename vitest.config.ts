8e9cda7d44f3bb8749f0295e54c35da5fb730cb3
stLmpp <gui.stlmpp@gmail.com>
2023-09-24 17:35:52 -0300

    fix: update config dependency to fix coverage include and exclude

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { vitestConfig } from '@assis-delivery/config';
+import { InlineConfig } from 'vitest';
 import { defineConfig, mergeConfig } from 'vitest/config';
 
 export default defineConfig(async (env) =>
   mergeConfig(await vitestConfig(env), {
     test: {
       setupFiles: ['vitest.setup.ts'],
-    },
+    } satisfies InlineConfig,
   }),
 );
